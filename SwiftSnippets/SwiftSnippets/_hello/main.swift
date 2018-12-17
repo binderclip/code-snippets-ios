7e51e3fcb92b43fdabf161514aed3deceec10f26
clip <binderclip2014@gmail.com>
2018-12-17 14:04:23 +0800

    hello

---
+++ SwiftSnippets/SwiftSnippets/_hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSnippets
+//
+//  Created by Roland Zhou on 2018/12/17.
+//  Copyright © 2018 Roland Zhou. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
